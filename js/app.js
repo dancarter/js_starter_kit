54d9ed5555cfe31d673c34b67a01a24f9261ef5d
Daniel Carter <geddoe@gmail.com>
2014-01-08 10:25:57 -0500

    Guess the number game

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-alert('Hey find me to know where to put the javascript');
+$(document).ready(function() {
+  var num = Math.floor((Math.random() * 100) + 1);
+  var guesses = 0;
+
+  $( ".submit" ).on( "click", function() {
+    var userGuess = parseInt($( "#guess" ).val());
+    if ( userGuess && userGuess >= 0 && userGuess <= 100 ) {
+      guesses += 1;
+      if (  userGuess === num ) {
+        $( ".submit" ).toggleClass( "hidden" )
+        $( ".replay" ).toggleClass( "hidden" )
+        document.getElementById("guess").disabled = true;
+        alert( "Congratulations, you've guessed the number in " + guesses + " guesses!" );
+      } else {
+        if ( userGuess > num ) {
+          alert( userGuess + " was too high, try again..." );
+        } else {
+          alert( userGuess + " was too low, try again..." );
+        }
+      }
+    } else {
+      alert( "Invalid input, must enter a number between 0 and 100." );
+    }
+  });
+
+  $( ".replay" ).on( "click", function() {
+    location.reload();
+  });
+
+})
